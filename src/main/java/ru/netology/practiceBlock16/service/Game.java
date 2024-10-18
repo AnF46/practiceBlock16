2acd7daec6245f1671fcc9fd74bbf2e8a62bbcff
AnF <anfwork007@gmail.com>
2024-10-18 23:07:48 +0300

    added code_v1

---
+++ src/main/java/ru/netology/practiceBlock16/service/Game.java
@@ -0,0 +1,11 @@
+package ru.netology.practiceBlock16.service;
+public class Game {
+
+    public void register (Player player){
+
+    }
+
+    public void round (String playerName1, String playerName2){
+
+    }
+}
